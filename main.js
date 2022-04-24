75df0f8c713c262e5993912ad69bee9ffa7e9e62
rodion.gordienko <rodion.gordienko@koroka.solutions>
2022-04-24 23:24:44 +0300

    config

--- main.js
+++ main.js
@@ -5,40 +5,76 @@ import config from 'config'
 import mainRouters from "./routes/main.routes.js"
 import cors from 'cors'
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+import dotenv from 'dotenv'
+import * as http from "http";
+dotenv.config()
 
 const app = express();
 
 app.use(cors())
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
 app.use(mainRouters)
 app.get('/', (req, res) => {
     res.status(404).sendFile(__dirname + '/index.html')
 })
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
     res.status(404).sendFile(__dirname + '/error404.html')
 })
 
-async function start() {
+const args = {}
+process.argv.forEach(arg => {
+    if(arg.includes('--')) {
+        const objItem = arg.replaceAll('--','').split('=')
+        args[objItem[0]] = objItem[1]
+    }
+})
+
+
+async function local() {
     try {
         await mongoose.connect(config.get('MONGO_URI'), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
-       app.listen(process.env.PORT || 5000, () => {
-            console.log(`Server has starter...`)
+        const port = config.get('PORT')
+        http.createServer(app).listen(port, '192.168.0.103', () => {
+            console.log(`
+            Server has starter on   [ http://localhost:${port} ]
+                                    [ http://192.168.0.103:${port} ]
+            `)
         })
-    }catch (e) {
-        console.error('Server error:',e)
+    } catch (e) {
+        console.error('Server error:', e)
         process.exit(1)
     }
 }
 
-start()
+async function heroku() {
+    try {
+        await mongoose.connect(config.get('MONGO_URI'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server has starter...`)
+        })
+    } catch (e) {
+        console.error('Server error:', e)
+        process.exit(1)
+    }
+}
+
+if(args.ENV === 'prod') {
+    heroku()
+}else {
+    local()
+}
