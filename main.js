b0e646978988c39efe73dae86ac2a7bfbd4decdb
rodion.gordienko <rodion.gordienko@koroka.solutions>
2022-04-24 22:41:18 +0300

    .

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ async function start() {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
-       app.listen(80, () => {
+       app.listen(config.get('port') || 3000, () => {
             console.log(`
             Server has starter on   [ http://localhost:${config.get('port')} ]
                                     [ http://192.168.0.103:${config.get('port')} ]
